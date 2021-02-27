cede19e5685c0ca0d3267e06f41edd586399a69d
ByJhona <jhonatantecno23@gmail.com>
2021-02-27 12:54:17 -0300

    Pasta img criada

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Image} from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
 export default function App() {
