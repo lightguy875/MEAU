e587d4f8cc807339a1f720f3e4cf42b3ce52aa73
ByJhona <jhonatantecno23@gmail.com>
2021-02-27 13:27:20 -0300

    Teste

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View , Image} from 'react-native';
+import { Button, StyleSheet, Text, View} from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Tela de Login</Text>
-      <Text>Test</Text>
-      <TextInput>Test</TextInput>
+
+      
+        <TextInput 
+            style={styles.input}
+            placeholder="Nome de usuário"
+        />
+
+        <TextInput
+            style={styles.input}
+            placeholder="Senha"
+            defaultValue=''
+        />
+
+        
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -21,4 +34,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+      borderBottomColor: 'black',
+      width: 300,
+      borderBottomWidth: 1,
+      
+  }
+
+
 });
