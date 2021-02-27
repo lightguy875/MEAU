e3d662b9c4d4708fd9463f830f5fa94e4b183a8c
ByJhona <jhonatantecno23@gmail.com>
2021-02-27 13:33:42 -0300

    Merge entre branches realizado

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { registerRootComponent } from 'expo';
 
-import App from './src/views/Login';
+import Login from './src/views/Login';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(Login);
--- src/views/Login.js
+++ src/views/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Button, StyleSheet, Text, View} from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
-export default function App() {
+export default function Login() {
   return (
     <View style={styles.container}>
 
