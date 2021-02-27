d22587348752c31bed3c1f795331c4289050f8fe
ByJhona <jhonatantecno23@gmail.com>
2021-02-27 14:24:35 -0300

    Botao de login adicionado

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text, View} from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity, Alert} from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
 export default function Login() {
+
+
   return (
     <View style={styles.container}>
 
@@ -15,10 +17,16 @@ export default function Login() {
 
         <TextInput
             style={styles.input}
+            secureTextEntry={true}
             placeholder="Senha"
-            defaultValue=''
         />
 
+        <TouchableOpacity
+            style={styles.botao}
+            
+        >
+            <Text style={styles.botaoText}>Login</Text>
+        </TouchableOpacity>
         
 
 
@@ -27,19 +35,45 @@ export default function Login() {
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-      borderBottomColor: 'black',
-      width: 300,
-      borderBottomWidth: 1,
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    input: {
+        borderBottomColor: 'black',
+        width: 300,
+        borderBottomWidth: 1,
+        padding: 8,
       
-  }
+    },
+
+    botao: {
+        backgroundColor: 'blue',
+        width: 232,
+        height: 40,
+        marginTop: 52,
+        padding: 5,
+        borderRadius: 5,
+        backgroundColor: '#88c9bf',
+
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+
+    botaoText: {
+        color: '#434343',
+    },
+
+
+
+
+
 
 
 });
