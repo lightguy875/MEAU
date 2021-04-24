0398ba63ef7cc8cc898aa01e6c02a37ad3e665ef
lightguy875 <luz875@gmail.com>
2021-04-24 20:36:03 -0300

    Estrutura básica do chat

--- src/views/Conversa.js
+++ src/views/Conversa.js
@@ -1,14 +1,17 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {
 Text,
 SafeAreaView,
 View,
 StyleSheet
 } from 'react-native'
+import {GiftedChat, Actions} from  'react-native-gifted-chat';
 import {useSelector} from 'react-redux'
 import auth from '@react-native-firebase/auth'
+import firestore from '@react-native-firebase/firestore'
 export default function Conversa({navigation,route}) {
 
+const [messages, setMessages] = useState([]);
 let usuario = useSelector(state => state.user)
 let dado = route.params.item.users.filter(elemento => {
     return elemento.name != usuario.user.name && elemento.nome_de_usuario != usuario.user.nome_de_usuario
@@ -20,17 +23,72 @@ id = id[0]
 dado = dado[0]
 dado = Object.assign(dado, {id: id})
 
+let user = {
+    id: auth().currentUser.uid,
+    name: usuario.user.name,
+    avatar: '',
+}
+
+
+
 useEffect( () => {
 
     navigation.setOptions({
         title: dado.name.split(' ').slice(0,2).join(' ')
     });
+
 },[route.params])
 
+async function carregar_mensagens() {
+    await firestore().collection('Chat').doc(route.params.item.id).collection('messages').orderBy('createdAt', 'desc').onSnapshot((querySnapshot) => {
+        var mensagens = [];
+        querySnapshot.forEach((doc) => {
+            mensagens.push(Object.assign(doc.data(), {id: doc.id}))
+        })
+        setMessages(mensagens)
+
+    })
+}
+
+
+const sendChatMessage = async(chat) => {
+
+console.log(chat)
+// await firestore().collection('Chat').doc(route.params.item.id).update({
+//     momento: chat.createdAt
+//     ultima_mensagem: chat.text
+// })
+//     return firestore()
+//       .collection('Chat')
+//       .doc(route.params.item.id)
+//       .collection('messages')
+//       .add(chat);
+  };
+
+  const onSend = async (msgs) => {
+    msgs.forEach((msg) => {
+      const { text, user } = msg;
+      const message = { text, user, createdAt: new Date().getTime()};
+      //createMessage(chatID, message);
+    //   const ids = chatID();
+      
+//console.log
+// ('Teste1: ', ids);
+      sendChatMessage(message);
+    });
+  };
+
+
+
+
+
+
+
     return(
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.texto}>Chegamos até a página de conversa</Text>
-        </SafeAreaView>
+        // <SafeAreaView style={styles.container}>
+        //     <Text style={styles.texto}>Chegamos até a página de conversa</Text>
+        // </SafeAreaView>
+        <GiftedChat messages={messages} user={user} onSend={onSend} />
     )
 
 }
